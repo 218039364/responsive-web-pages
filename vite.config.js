510c669e05b617c8f09b89a0849a165fb9000a57
Tebogo Johannes <tebogomojela01@gmail.com>
2023-06-01 01:14:16 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json" ;
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input : {
+        main: 'index.html',
+        sub1: 'index-bootstrap.html',
+        sub2: 'index-materialize.html',
+      }
+    },
+  },
+  base : '/responsive_webpages/',
+  plugins: [handlebars({
+    context : {
+      data
+  }
+})],
+})
+
+
+
